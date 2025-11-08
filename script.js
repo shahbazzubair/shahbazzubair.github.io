7b6feb18937def238daad49a594569e027fff8d2
Muhammad Shahbaz <theshahbazzubair@gmail.com>
2025-11-08 16:52:08 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
